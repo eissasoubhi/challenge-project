4a7ed1687d807450c5f447281004e4ed21a40c00
EWin <eissa.soubhi@gmail.com>
2018-03-03 23:02:14 +0000

    Add login and register views and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import HfRegister from '@/views/Register'
+import HfLogin from '@/views/Login'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
+    {
+      name: 'register',
+      path: '/register',
+      component: HfRegister
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: HfLogin
+    }
   ]
 })
